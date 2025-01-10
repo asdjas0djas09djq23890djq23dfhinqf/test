5aaec277169510bfe1b4d7ff34975e77587efb3d
BT_1N3_30 <jonahm28@nycstudents.net>
2025-01-10 13:46:41 -0500

    newclass

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("our first shared repo");
         System.out.println("this is the other partner adding some code");
+        System.out.println("whats up");
     }
 
 }
