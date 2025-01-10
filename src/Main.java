8a6fc9291f142061f6a13c3315dfe884f0abbc1d
BT_1N3_30 <jonahm28@nycstudents.net>
2025-01-10 13:57:56 -0500

    new 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("askdasdasdoasdasduasdas;");
         System.out.println("this is the other partner adding some code");
-        System.out.println("whats up");
+        System.out.println("poop");
         System.out.println("qt");
     }
 }
