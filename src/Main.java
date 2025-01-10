4f70b0b9b402955b77b2b8d9702d39e82176a635
BT_1N3_30 <jonahm28@nycstudents.net>
2025-01-10 13:58:29 -0500

    new 3

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.print("askdasdasdoasdasduasdas;");
         System.out.println("this is the other partner adding some code");
         System.out.println("poop");
-        System.out.println("qt");
+        System.out.println("123");
     }
 }
